e57a139452c16b2d61ae645ebce7e01455fbec70
vithal <vithalchoudhary4@gmail.com>
2025-04-09 16:26:52 +0530

    assets

--- Assets/_script_/Delivery.cs
+++ Assets/_script_/Delivery.cs
@@ -8,8 +8,12 @@ public class Delivery : MonoBehaviour
     bool hasPackage;
     [SerializeField] Color32 hasColorPackage=new Color32(1,1,1,1);
     [SerializeField] Color32 noColorPackage = new Color32(1, 1, 1, 1);
-
     [SerializeField] float Destroy_Time = 1.0f;
+    SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("shittt");
@@ -19,11 +23,16 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Package2" && !hasPackage)
-        { Debug.Log(" package is picked"); hasPackage = true; Destroy(other.gameObject, Destroy_Time); }
+        { Debug.Log(" package is picked"); 
+            hasPackage = true;
+            Destroy(other.gameObject, Destroy_Time); 
+            spriteRenderer.color = hasColorPackage;
+        }
 
         if (other.tag == "Loc" && hasPackage)   
         { Debug.Log("delivered");
             hasPackage = false;
+            spriteRenderer.color = noColorPackage;
         }
     }
     
